0b807efe43212eac7cd88a1b8ab897e9febd08d6
abctech10946 <abctech10946@gmail.com>
2019-01-15 00:27:39 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/vim $HOME/.vim
 ln -sf $HOME/.dotfiles/conky $HOME/.conky
 ln -sf $HOME/.dotfiles/conkyrc $HOME/.conkyrc
+ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
