7aa9e1c0b4d78c34f477ebd1b5a8be647b6f9ea4
abctech10946 <abctech10946@gmail.com>
2020-02-11 11:01:43 +0900

    delete conky config

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/vim $HOME/.vim
-ln -sf $HOME/.dotfiles/conky $HOME/.conky
-ln -sf $HOME/.dotfiles/conkyrc $HOME/.conkyrc
 ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
